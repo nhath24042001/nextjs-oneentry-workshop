3ea3e76ee6370cf8eab924bd63ef1f7d852492c3
nhath24042001 <nhath24042001@gmail.com>
2025-06-04 16:05:16 +0700

    feat: add login and signup pages with form components and background images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css';
-import { Header } from '@/components/shared/Header';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
@@ -12,7 +11,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="vi">
       <body className="text-black dark:text-white transition-colors duration-300 ease-in-out">
-        <Header />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,6 @@
-import { WorkshopCard } from '@/components/shared/WorkshopCard'
-import { Workshop } from '@/typings';
+import { redirect } from 'next/navigation';
 
-const mockData: Workshop[] = [
-  {
-    id: '1',
-    title: 'Thiết kế UI với Figma',
-    description: 'Hướng dẫn tạo UI chuyên nghiệp.',
-    date: '2025-06-15T09:00:00Z',
-    price: 200000,
-    location: 'TP. HCM',
-    image: '',
-  },
-  {
-    id: '2',
-    title: 'Lập trình React cơ bản',
-    description: 'Khóa học lập trình React từ cơ bản đến nâng cao.',
-    date: '2025-07-01T10:00:00Z',
-    price: 300000,
-    location: 'Hà Nội',
-    image: '',
-  },
-  {
-    id: '3',
-    title: 'Kỹ năng giao tiếp hiệu quả',
-    description: 'Phát triển kỹ năng giao tiếp trong công việc.',
-    date: '2025-08-20T14:00:00Z',
-    price: 150000,
-    location: 'Đà Nẵng',
-    image: '',
-  },
-  {
-    id: '4',
-    title: 'Khóa học Python cơ bản',
-    description: 'Học lập trình Python từ những kiến thức cơ bản.',
-    date: '2025-09-10T11:00:00Z',
-    price: 250000,
-    location: 'Cần Thơ',
-    image: '',
-  },
-  {
-    id: '5',
-    title: 'Thiết kế đồ họa với Adobe Illustrator',
-    description: 'Khóa học thiết kế đồ họa chuyên nghiệp.',
-    date: '2025-10-05T13:00:00Z',
-    price: 180000,
-    location: 'Nha Trang',
-    image: '',
-  },
-];
+export default function Page() {
 
-export default function HomePage() {
-  return (
-    <main className="bg-muted dark:bg-zinc-950 text-gray-800 dark:text-white min-h-screen">
-      <div className="max-w-6xl mx-auto px-4 py-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 min-h-[200px]">
-          {mockData.map((workshop) => (
-            <WorkshopCard key={workshop.id} workshop={workshop} />
-          ))}
-        </div>
-      </div>
-    </main>
-  );
+  redirect('/auth/login');
 }
